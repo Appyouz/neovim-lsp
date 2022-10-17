962820eccf84da85be7985859f9ef9e14ab5522e
Appyouz <Dikpalkhatri7@gmail.com>
2022-10-17 18:15:26 +0545

    removed all

--- init.lua
+++
@@ -1,7 +0,0 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorschemes"
-require "user.lsp.lsp"
-require "user.lsp.null-ls"
-require "user.lsp.lspsaga"
--- lua/user/colorschemes.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[
-try
-  colorscheme onedark
-  set background=dark
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
