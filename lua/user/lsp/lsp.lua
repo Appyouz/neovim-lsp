fe838b846ac3de66c1fa95cd177f004c9cbb587c
Appyouz <91145436+Appyouz@users.noreply.github.com>
2022-10-26 18:37:59 +0545

    clangd.lua removed at the moment

--- lua/user/lsp/lsp.lua
+++ lua/user/lsp/lsp.lua
@@ -1,7 +1,7 @@
 local status, nvim_lsp = pcall(require, "lspconfig")
-if (not status) then return end
-
-
+if not status then
+	return
+end
 
 local cwd = vim.loop.cwd
 
@@ -12,124 +12,102 @@ local opts = { noremap = true, silent = true }
 --vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  --vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	--vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
 
-
-vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-      border = 'rounded',
-      source = 'always',
-      prefix = ' ',
-      scope = 'cursor',
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
 end
 
-
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
-require('lspconfig')['pyright'].setup {
-  on_attach = on_attach,
-  flags = lsp_flags,
-  root_dir = cwd,
-}
-require('lspconfig')['tsserver'].setup {
-  on_attach = on_attach,
-  flags = lsp_flags,
-  root_dir = cwd,
-}
-require('lspconfig')['sumneko_lua'].setup {
-  on_attach = on_attach,
-  flags = lsp_flags,
-  root_dir = cwd,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-    },
-  },
-}
+require("lspconfig")["pyright"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+	root_dir = cwd,
+})
+require("lspconfig")["tsserver"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+	root_dir = cwd,
+})
+require("lspconfig")["sumneko_lua"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+	root_dir = cwd,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+		},
+	},
+})
 
---[[ 
-  require('lspconfig')['clangd'].setup {
-      cmd = { "clangd" },
-      filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-      single_file_support = true,
-      on_attach = on_attach,
-      capabilities = capabilities,
-      root_dir = cwd,
-    } ]]
-   
+require("lspconfig")["clangd"].setup({
+	cmd = { "clangd" },
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+	single_file_support = true,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = cwd,
+})
 
-require('lspconfig')['emmet_ls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = cwd,
-}
+require("lspconfig")["emmet_ls"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = cwd,
+})
 
-require('lspconfig')['html'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = cwd,
-}
+require("lspconfig")["html"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = cwd,
+})
 
-require('lspconfig')['marksman'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = cwd,
-  cmd = { "marksman", "server" },
-  filetypes = { "markdown" },
-  single_file_support = true,
-}
-
+require("lspconfig")["marksman"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = cwd,
+	cmd = { "marksman", "server" },
+	filetypes = { "markdown" },
+	single_file_support = true,
+})
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
@@ -141,37 +119,33 @@ local function lsp_highlight_document(client)
 	-- end
 end
 
-
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
-}
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	update_in_insert = false,
+	virtual_text = { spacing = 4, prefix = "●" },
+	severity_sort = true,
+})
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●'
-  },
-  update_in_insert = true,
-  underline = true,
+	virtual_text = {
+		prefix = "●",
+	},
+	update_in_insert = true,
+	underline = true,
 	severity_sort = true,
-  float = {
-    source = "always", -- Or "if_many"
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    header = "",
-    prefix = "",
-  },
+	float = {
+		source = "always", -- Or "if_many"
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		header = "",
+		prefix = "",
+	},
 })
